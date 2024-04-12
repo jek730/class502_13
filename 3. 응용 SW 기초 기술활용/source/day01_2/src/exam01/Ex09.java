20fc661426d97b27e3cec6e027add054907785ca
jek730 <jejek730@gmail.com>
2024-04-12 16:21:06 +0900

    변수와 자료형 실습

---
+++ 3. 응용 SW 기초 기술활용/source/day01_2/src/exam01/Ex09.java
@@ -0,0 +1,11 @@
+package exam01;
+public class Ex09 {
+    public static void main(String[] args) {
+        long num1 = 1000000000000L;  //8
+        double num2 = num1;  // 정수 -> 실수 : 자동 형변환(묵시적 형변환) - 8
+        float num3 = num1;
+
+        double num4 = 0.0;
+        for (int i = )
+    }
+}
