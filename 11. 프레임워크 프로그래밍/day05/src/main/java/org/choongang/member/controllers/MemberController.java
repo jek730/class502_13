ac62a1250c70b33747e986cd78289fac382ede4a
jek730 <jejek730@gmail.com>
2024-07-15 14:52:10 +0900

    스프링 웹 MVC - 커맨드 객체 검증

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
@@ -2,18 +2,25 @@ package org.choongang.member.controllers;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.choongang.member.services.JoinService;
+import org.choongang.member.validators.JoinValidator;
+import org.choongang.member.validators.LoginValidator;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
 
-    //private final JoinValidator joinValidator;
+    private final JoinValidator joinValidator;
+    private final JoinService joinService;
 
+    private final LoginValidator loginValidator;
 
     @GetMapping("/join")
     public String join(@ModelAttribute RequestJoin form) {
@@ -24,17 +31,40 @@ public class MemberController {
     @PostMapping("/join")
     public String joinPs(@Valid RequestJoin form, Errors errors) {
         // 회원 가입 데이터 검증
-        //joinValidator.validate(form, errors);
+        joinValidator.validate(form, errors);
 
         if (errors.hasErrors()) { // reject, rejectValue가 한번이라도 호출되면 true
             return "member/join";
         }
 
+        joinService.process(form); // 회원 가입 처리
+
         return "redirect:/member/login";
     }
 
+
+    @GetMapping("/login")
+    public String login(@ModelAttribute RequestLogin form) {
+
+        return "member/login";
+    }
+
+
+    @PostMapping("/login")
+    public String loginPs(@Valid RequestLogin form, Errors errors) {
+
+        loginValidator.validate(form, errors);
+
+        if (errors.hasErrors()) {
+            return "member/login";
+        }
+
+        return "redirect:/";
+    }
+
+    /*
     @InitBinder
     public void initBinder(WebDataBinder binder) {
-
-    }
+        binder.setValidator(joinValidator);
+    }*/
 }
--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/mappers/MemberMapper.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/mappers/MemberMapper.java
@@ -10,6 +10,5 @@ public interface MemberMapper {
     int register(Member member);
 
     Member get(String email);
-    @Select("SELECT COUNT(*) FROM MEMBER WHERE email=#{email}")
     int exists(String email);
 }
